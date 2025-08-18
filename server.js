0214d06297dab6fac4d63a10438e163c77aa1d90
DavideCriscuolo <davide.alcatel@gmail.com>
2025-08-18 12:23:32 +0200

    debug

--- server.js
+++ server.js
@@ -2,35 +2,26 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
-import path from "path";
 import gymRouter from "./routes/gym.js";
 import connection from "./db/connection.js";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const port = process.env.PORT || 5000;
 
 const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
 app.use(
   cors({
-    origin: "https://sito-palestra-lilac.vercel.app", // il tuo frontend su Vercel
+    origin: "https://sito-palestra-lilac.vercel.app", // frontend Vercel
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true, // solo se vuoi usare cookie o auth
+    // credentials: true // solo se usi cookie
   })
 );
-
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(express.json());
+
+// Route API
 app.use("/gym", gymRouter);
 
-// Serve frontend Vite (file statici)
-app.use(express.static(path.join(__dirname, "dist")));
-
-// Catch-all per React Router
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
+// Test connessione DB
 app.get("/test-db", (req, res) => {
   connection.query("SELECT 1+1 AS result", (err, results) => {
     if (err) return res.status(500).send(err);
