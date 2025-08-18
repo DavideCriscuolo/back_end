deba930e172938358a2a1da01f1057ec6d245dbe
DavideCriscuolo <davide.alcatel@gmail.com>
2025-08-18 10:28:23 +0200

    per debug

--- db/connection.js
+++ db/connection.js
@@ -1,24 +1,15 @@
-import mysql from "mysql2";
+import mysql from "mysql";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const credentials = {
+const connection = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PW,
   database: process.env.DB_NAME,
-};
-
-const connection = mysql.createConnection(credentials);
-
-console.log(connection);
-
-connection.connect((err) => {
-  if (err) {
-    console.log(err.sqlMessage);
-  } else {
-    console.log("Connection Success");
-  }
+  connectionLimit: 10,
 });
+
 export default connection;
+console.log(connection);
