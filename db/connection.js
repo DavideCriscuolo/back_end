d7d736e6e7a19c6ce0eba976ccebfa124571b7c6
DavideCriscuolo <davide.alcatel@gmail.com>
2025-08-18 12:01:19 +0200

    debug per conessione a db

--- db/connection.js
+++ db/connection.js
@@ -4,13 +4,24 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const connection = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQLPORT,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PW,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
   connectionLimit: 10,
+  queueLimit: 0,
+});
+
+// Test connessione
+connection.getConnection((err, conn) => {
+  if (err) {
+    console.error("Errore connessione al DB:", err);
+  } else {
+    console.log("Connessione al DB riuscita!");
+    conn.release();
+  }
 });
 
 export default connection;
-console.log(connection);
