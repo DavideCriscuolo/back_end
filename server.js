9fb58070d39e791369dc92c310c3232ce70f7f4f
DavideCriscuolo <davide.alcatel@gmail.com>
2025-11-09 10:43:28 +0100

    modifica al cors

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-// app.use(
-//   cors({
-//     origin: "https://sito-palestra-lilac.vercel.app", // frontend Vercel
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     // credentials: true // solo se usi cookie
-//   })
-// );
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://sito-palestra-lilac.vercel.app", // frontend Vercel
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true, // solo se usi cookie
+  })
+);
+//app.use(cors());
 app.use(express.json());
 
 // Route API
